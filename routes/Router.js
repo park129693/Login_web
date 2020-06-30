a0aae811128aa1e7a902df17e2f057deec14356a
park129693 <park129693@gmail.com>
2020-06-30 17:20:57 +0900

    로그인 페이지와 로그인 성공시 index 페이지로 이동

--- routes/Router.js
+++ routes/Router.js
@@ -38,21 +38,30 @@ router.post('/signup',(req, res, next)=>{
     })
 })
 
-router.post('/insert', (req, res, next)=>{
-
-    var contact = new User()
-    contact.username = req.body.username
-    contact.passwordHash = req.body.passwordHash
-    contact.email = req.body.email
-    
-    contact.save((err, result)=>{
-        if(err) {
-            console.log(err)
-        }
-        console.log(result)
-        res.send("Success")
-    })
-
+router.get('/login',(req, res , next)=>{
+    res.render('login')
 })
+
+router.post('/login',async (req, res, next) => {
+    var username = await req.body.username
+    var passwordHash = await req.body.passwordHash
+    User.findOne({username:username} ,(err,result) =>{
+        if(err){
+            console.log(err.body)
+        }
+        if(!result){
+            res.send(`${username} is not exist`)
+        } else {
+            if(result.passwordHash == passwordHash){
+                console.log(username)
+                res.render('index', {data:username})
+            }
+            else{
+                res.send(`${username}'s password is wrong`)
+            }
+        }
+    })
+})
+
 
 module.exports = router;
