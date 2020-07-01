02719f8b1324f28a7e78561454595b4fa4526dff
park129693 <park129693@gmail.com>
2020-07-01 15:44:23 +0900

    my Coding

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ mongoose.Promise = global.Promise
 var flash = require("express-flash-messages")
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
+var passport = require('passport')
+var User = require('./models/User')
 
 var pw = process.env.PASSWORD
 var url = `mongodb+srv://root:${pw}@cluster0.ze1q8.mongodb.net/MyDB_Daejeon?retryWrites=true&w=majority`
@@ -21,13 +23,45 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
-app.user(session({
+app.use(cookieParser())
+app.use(session({
     secret:"fhweohfowejh",
     resave:true,
     saveUninitialized:true
 }))
 
-app.use('/', apiRouter)
+app.use(flash())
+
+//passprot
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser((user, done)=>{
+    done(null, user._id)
+})
+
+passport.deserializeUser((userId, done)=>{
+    User.findById(userId, (err, user)=>{
+        done(err, user)
+    })
+})
+
+var localStrategy = require("passport-local")
+var local = new localStrategy((username, password, done)=>{
+    User.findOne({username})
+    .then(user =>{
+        if(!user || user.validPassword(password)) {
+            done(null, false, {meassage:"Invalid username /password"})
+        } else {
+            done(null, user)
+        }
+    })
+    .catch(e=>done(e))
+})
+
+passport.use("local", local)
+//routin 파일 미들웨어로 등록
+app.use('/', apiRouter(passport))
 
 const port = process.env.PORT
 app.listen(port, ()=>{
--- routes/Router.js
+++ routes/Router.js
@@ -4,150 +4,155 @@ var User = require('../models/User')
 var List = require('../models/List')
 var bcrypt = require('bcryptjs')
 
-var name = {
-    a: "철수",
-    b: "바둑이",
-    c: "영희"
+function loggedInOnly(req, res, next) {
+    if(req.isAuthenticated()) {
+        next()
+    }
+    res.redirect('/login')
 }
-router.get('/', (req, res, next)=>{
-    User.find((err, result)=>{
-        if(err) {
-            console.log(error)
-        }
-        // console.log(req)
-        //res.send(result)
-        res.render('index', {data:result})
+
+function loggedOutOnly(req, res, next) {
+    if(req.isUnauthenticated()) {
+        next()
+    }
+    res.send('sdfsdfdsfsdfsdf')
+}
+
+function authenticate(passport) {
+    
+    router.use((req, res, next)=>{
+        res.locals.currentUser = req.user
+        console.log(res.locals.currentUser)
+        next()
     })
 
-})
 
-router.get('/signup', (req, res, next)=>{
-    res.render('signup', {message:"ture"})
-})
+    router.get('/', loggedInOnly,  (req, res, next)=>{
+        User.find((err, result)=>{
+            if(err) {
+                console.log(error)
+            }
+            // console.log(req)
+            //res.send(result)
+            res.render('index', {data:result})
+        })
 
-// router.post('/signup',(req, res, next)=>{
-//     User.findOne({username :req.body.username}, (err, user)=>{
-//         if(err) {
-//             console.log(err)
-//         } else if(user) {
-//             res.render('signup', {message:"false", data:user.username})
-//         }
-//         var contact = new User()
-//         contact.username = req.body.username
-//         contact.passwordHash = bcrypt.hashSync(req.body.passwordHash)
-//         contact.email = req.body.email
-    
-//         contact.save((err, result)=>{
-//             if(err) {
-//                 console.log(err)
-//             }
-//             console.log(result)
-//             res.redirect('/main')
-//         })
-//     })
-// })
+    })
 
-router.post('/signup', (req, res , next)=>{
-    User.findOne({username :req.body.username} ,async (err, user)=>{
-        if(err) {
-            console.log(err)
-        } 
-        if(user) {
-            console.log(user)
-            res.render('signup',  {message:"false" ,data:user.username})
-        } 
-        var username = await req.body.username
-        var passwordHash =await bcrypt.hashSync(req.body.passwordHash)
-        var email = await req.body.email
-        console.log(bcrypt.compareSync(req.body.passwordHash,passwordHash ))
-        // if (user) {
-        //   res.status(400).send({ message: "Email already exist" }).end();
-        // }
-        await User.create({ username,passwordHash, email });
-        res.redirect('/main')
+    router.get('/signup', loggedOutOnly,(req, res, next)=>{
+        res.render('signup', {message:"ture"})
+    })
+
+    router.post('/signup', loggedOutOnly, async (req, res , next)=>{
+            var username = await req.body.username
+            var passwordHash = await req.body.passwordHash
+            var email = await req.body.email
+            console.log(bcrypt.compareSync(req.body.passwordHash,passwordHash ))
+
+        User.findOne({username :req.body.username} ,(err, user)=>{
+            if(err) {
+                console.log(err)
+            } 
+            if(user) {
+                console.log(user)
+                res.render('signup',  {message:"false" ,data:user.username})
+            } 
+            
+            User.create({ username,passwordHash, email })
+            .then(user=>{
+                req.login((user, err)=>{
+                    if(err) {
+                        next(err)
+                    } else {
+                        res.redirect('/main')
+                    }
+                })
+            })
+            .catch(e=> {
+                if(e.name == "ValidationError") {
+                    res.redirect("/signup")
+                }
+                next(e);
+            })
+        })         
+    })
+
+
+    router.get('/login', loggedOutOnly,(req, res , next)=>{
+        res.render('login')
+    })
+
+    router.post('/login', loggedOutOnly ,
+
+        passport.authenticate("local", {
+            successRedirect:"/main",
+            failureRedirect:'login',
+            failureFlash:true
+        })
+    //     async (req, res, next) => {
+    //     var username = await req.body.username
+    //     var passwordHash = await req.body.passwordHash
+    //     User.findOne({username:username} ,(err,result) =>{
+    //         if(err){
+    //             console.log(err.body)
+    //         }
+    //         if(!result){
+    //             res.send(`${username} is not exist`)
+    //         } else {
+    //             if(result.passwordHash == passwordHash){
+    //                 console.log(username)
+    //                 res.render('index', {data:username})
+    //             }
+    //             else{
+    //                 res.send(`${username}'s password is wrong`)
+    //             }
+    //         }
+    //     })
+    // }
+    )
+
+    router.get('/main', loggedInOnly,(req, res, next)=>{
+        List.find((err, result)=>{
+            if(err) {
+                console.log(err)
+                res.status(500).send("Interenal is delay!!")
+            }
+            console.log(result[0].title)
+            res.render('main', {data:result})
+        })
+    })
+
+    router.get('/insert', loggedInOnly, (req, res, next)=>{
+        res.render('insert')
+    })
+
+    router.post('/insert', loggedInOnly, (req, res, next)=>{
+        var contact = new List()
+        contact.title = req.body.title
+        contact.description = req.body.description
+        contact.email = req.body.email
+        contact.author = req.body.author
+        
+        contact.save((err, result)=>{
+            if(err) {
+                console.log(err)
+            }
+            console.log(result)
+            res.redirect("/main")
+        })
+    })
+
+    router.all("/logout",(req, res, next)=>{
+        req.logout()
+        res.redirect('/login')
+    })
+
+    //Error Handler
+    router.use((err, req, res)=>{
+        console.error(err.stack)
     })   
-})
-
-
-router.get('/login',(req, res , next)=>{
-    res.render('login')
-})
-
-router.post('/login',async (req, res, next) => {
-    var username = await req.body.username
-    var passwordHash = await req.body.passwordHash
-    User.findOne({username:username} ,(err,result) =>{
-        if(err){
-            console.log(err.body)
-        }
-        if(!result){
-            res.send(`${username} is not exist`)
-        } else {
-            if(result.passwordHash == passwordHash){
-                console.log(username)
-                res.render('index', {data:username})
-            }
-            else{
-                res.send(`${username}'s password is wrong`)
-            }
-        }
-    })
-})
-
-// router.route('/login')
-//     .get((req, res, next) => {
-//         res.render('login.ejs')
-//     })
-//     .post(async (req, res, next) => {
-//         var username = await req.body.username
-//         var passwordHash = await req.body.passwordHash
-//         User.findOne({username:username} ,(err,result) =>{
-//             if(err){
-//                 console.log(err.body)
-//             }
-//             if(!result){
-//                 res.send(`${username} is not exist`)
-//             } else {
-//                 if(result.passwordHash == passwordHash){
-//                     console.log(username)
-//                     res.render('index.ejs', {data:username})
-//                 }
-//                 else{
-//                     res.send(`${username}'s password is wrong`)
-//                 }
-//             }
-//         })
-//     })
-
-router.get('/main',(req, res, next)=>{
-    List.find((err, result)=>{
-        if(err) {
-            console.log(err)
-            res.status(500).send("Interenal is delay!!")
-        }
-        console.log(result[0].title)
-        res.render('main', {data:result})
-    })
-})
-
-router.get('/insert',(req, res, next)=>{
-    res.render('insert')
-})
-
-router.post('/insert',(req, res, next)=>{
-    var contact = new List()
-    contact.title = req.body.title
-    contact.description = req.body.description
-    contact.email = req.body.email
-    contact.author = req.body.author
     
-    contact.save((err, result)=>{
-        if(err) {
-            console.log(err)
-        }
-        console.log(result)
-        res.redirect("/main")
-    })
-})
+    return router
+}
 
-module.exports = router;
+module.exports = authenticate;
